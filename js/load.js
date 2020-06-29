87176d98a8acbb4f7e5a0c32c7892775df93522e
Moe_Nya <1404058996@qq.com>
2020-06-29 22:48:17 +0800

    侧标栏更新

--- js/load.js
+++ js/load.js
@@ -32,18 +32,19 @@ function init()
     </div>\
 </div>";
 
-    mid.innerHTML = "<div class = \"midimfor\" id = \"mid\">\
+    mid.innerHTML = "<div class = \"main\">\
+                    <div class = \"midimfor\">\
                         <div class = \"myimformation\">\
                         <div class = \"head\"></div>\
                         <div class = \"talk\"><p><span style=\"color:red\">写代码是热爱，</span><span style=\"color:turquoise\">写到世界充满爱！</span></p></div>\
                     </div>\
                     <div class = \"midAboutMe\">\
                         <i class = \"midAboutMeStyle\"></i>\
-                        <a class = \"a4\" href = \"http://wpa.qq.com/msgrd?v=3&uin=1404058996&site=qq&menu=yes\" target = \"_blank\">&nbsp&nbsp&nbsp&nbspQQ</a>\
+                        <a class = \"a4\" href = \"http://wpa.qq.com/msgrd?v=3&uin=1404058996&site=qq&menu=yes\" target = \"_blank\">&nbsp&nbspQQ</a>\
                     </div>\
                     <div class = \"midAboutMe\">\
                         <i class = \"midAboutMeStyle2\"></i>\
-                        <a class = \"a4\" href = \"https://space.bilibili.com/8522226\" target = \"_blank\">&nbsp&nbsp&nbspBliBli</a>\
+                        <a class = \"a4\" href = \"https://space.bilibili.com/8522226\" target = \"_blank\">&nbsp&nbspBliBli</a>\
                     </div>\
                     <div class = \"midAboutMe\">\
                         <i class = \"midAboutMeStyle3\"></i>\
@@ -52,6 +53,13 @@ function init()
                     <div class = \"midAboutMe\">\
                         <i class = \"midAboutMeStyle4\"></i>\
                         <a class = \"a4\" href = \"https://twitter.com/C_Moe_Nya_N\" target = \"_blank\">&nbsp&nbspTwitter</a>\
+                    </div>\
+                    <div class = \"midAboutMe2\">\
+                        <div class = \"midAboutMeStyle5\">\
+                        111\
+                        </div>\
+                    </div>\
+                    </div>\
                     </div>\
                     ";
 }
