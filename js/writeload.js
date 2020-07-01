19bce4d216eb4af9d9c25e5724fa6533a15a9935
Moe_Nya <1404058996@qq.com>
2020-07-01 22:53:47 +0800

    样式更新

--- js/writeload.js
+++ js/writeload.js
@@ -2,7 +2,9 @@ $(document).ready(function (){
     var writebox = document.getElementById("writebox");
 
     writebox.innerHTML = "<div class = \"write\">\
+                        <article class = \"theArticle\">\
                         \
+                        </article>\
                         </div>\
                         <div class = \"write\"></div>\
                         <div class = \"write\"></div>\
