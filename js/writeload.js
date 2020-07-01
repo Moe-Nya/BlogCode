acf5b958f252acf6b9f659c5a8659d217c64ad91
Moe_Nya <33063741+Moe-Nya@users.noreply.github.com>
2020-07-02 01:42:37 +0800

    样式更新

--- js/writeload.js
+++ js/writeload.js
@@ -3,13 +3,51 @@ $(document).ready(function (){
 
     writebox.innerHTML = "<div class = \"write\">\
                         <article class = \"theArticle\">\
-                        \
+                        <h1 class = \"articleH1\"><a href = \"#\">搭建一个TeamSpeak3服务器</a></h1>\
+                        <span class = \"time\"><i class = \"articleIcon\"></i>&nbsp发表于&nbsp2020-06-24&nbsp•&nbsp<i class = \"wordNum\"></i>&nbsp字数统计&nbsp445</span>\
+                        <p class = \"articleP\">TS占用资源和带宽比较少，于是我想搞一个，反正服务器压力不大。</p>\
+                        <div class = \"read\"\ id = \"read\"\><span>阅读全文»</span><div>\
                         </article>\
                         </div>\
-                        <div class = \"write\"></div>\
-                        <div class = \"write\"></div>\
-                        <div class = \"write\"></div>\
-                        <div class = \"write\"></div>\
-                        <div class = \"write\"></div>\
+                        <div class = \"write\">\
+                        <article class = \"theArticle\">\
+                        <h1 class = \"articleH1\"><a href = \"#\">天气之子：为了爱不顾一切</a></h1>\
+                        <span class = \"time\"><i class = \"articleIcon\"></i>&nbsp发表于&nbsp2019-11-02&nbsp•&nbsp<i class = \"wordNum\"></i>&nbsp字数统计&nbsp1678</span>\
+                        <p class = \"articleP\">《你的名字》上映三年后，旧（新）海诚的新作《天气之子》也来啦~，我还是去看了首映，一个人。这次的作品更加成熟质量Max，也因此在日本冲破130亿日元的票房。</p>\
+                        <div class = \"read\"\ id = \"read\"\><span>阅读全文»</span><div>\
+                        </article>\
+                        </div>\
+                        <div class = \"write\">\
+                        <article class = \"theArticle\">\
+                        <h1 class = \"articleH1\"><a href = \"#\">你好，爱拍！ 再见，爱拍。</a></h1>\
+                        <span class = \"time\"><i class = \"articleIcon\"></i>&nbsp发表于&nbsp2019-10-01&nbsp•&nbsp<i class = \"wordNum\"></i>&nbsp字数统计&nbsp1456</span>\
+                        <p class = \"articleP\">今天来聊聊爱拍原创这个网站，2009年，爱拍原创为当时的中国互联网注入了新的血液。作为国内早期原创视频网站，爱拍在当时是一个重量级网站。</p>\
+                        <div class = \"read\"\ id = \"read\"\><span>阅读全文»</span><div>\
+                        </article>\
+                        </div>\
+                        <div class = \"write\">\
+                        <article class = \"theArticle\">\
+                        <h1 class = \"articleH1\"><a href = \"#\">KFC兼职：人生中的第一桶金</a></h1>\
+                        <span class = \"time\"><i class = \"articleIcon\"></i>&nbsp发表于&nbsp2019-09-30&nbsp•&nbsp<i class = \"wordNum\"></i>&nbsp字数统计&nbsp1522</span>\
+                        <p class = \"articleP\">首先想给各位跃跃欲试做兼职的学生党们一个建议，不要去KFC。</p>\
+                        <div class = \"read\"\ id = \"read\"\><span>阅读全文»</span><div>\
+                        </article>\
+                        </div>\
+                        <div class = \"write\">\
+                        <article class = \"theArticle\">\
+                        <h1 class = \"articleH1\"><a href = \"#\">Microsoft surface pro 6 苏菲6初体验</a></h1>\
+                        <span class = \"time\"><i class = \"articleIcon\"></i>&nbsp发表于&nbsp2019-09-27&nbsp•&nbsp<i class = \"wordNum\"></i>&nbsp字数统计&nbsp972</span>\
+                        <p class = \"articleP\">刚刚高中毕业的博主也陷入了新设备选购的困难（贫穷蒙蔽了我的双眼），但是和别人不一样的是，我并不是要一个游戏级的设备，而是一个办公级别的设备。</p>\
+                        <div class = \"read\"\ id = \"read\"\><span>阅读全文»</span><div>\
+                        </article>\
+                        </div>\
+                        <div class = \"write\">\
+                        <article class = \"theArticle\">\
+                        <h1 class = \"articleH1\"><a href = \"#\">K-ON！ 回响！</a></h1>\
+                        <span class = \"time\"><i class = \"articleIcon\"></i>&nbsp发表于&nbsp2019-09-25&nbsp•&nbsp<i class = \"wordNum\"></i>&nbsp字数统计&nbsp1347</span>\
+                        <p class = \"articleP\">5月13号，B站买回轻音后我也是把这部动画又看了一遍，我也不知道这是我看的第几遍了，轻音是我唯一一个多刷了的动画。</p>\
+                        <div class = \"read\"\ id = \"read\"\><span>阅读全文»</span><div>\
+                        </article>\
+                        </div>\
                         ";
 });
